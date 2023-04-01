90908443247dd26b4c8e0304e3cbd11396f60695
Thai <thaiphat_s@cmu.ac.th>
2023-04-01 21:55:05 +0700

    Update server.js
    Add a function to insert dummy data into the database every time

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+injectDummyDB()
+
 // Value for tabs
 const queueTab = {
     tabTitle: 'Queue',
@@ -131,30 +133,19 @@ app.listen("3000", () => {
 
 function injectDummyDB(){
     User.insertMany([{
-        "_id": {
-          "$oid": "642651c785069fdfe805d52a"
-        },
         "username": "admin",
         "password": "1234"
       }]);
     Order.insertMany([{
-        "_id": {
-          "$oid": "64252c7993e4cc92d4173992"
-        },
         "id": 1,
         "customer": "John Doe",
-        "time": {
-          "$date": "2011-04-20T02:30:51.010Z"
-        },
+        "time": Date.now(),
         "address": "Robert Robertson, 1234 NW Bobcat Lane, St. Robert, MO 65584-5678",
         "telNo": "0832221155",
         "items": [
           {
             "name": "Chicken curry",
             "price": 60,
-            "_id": {
-              "$oid": "64252c7993e4cc92d4173993"
-            },
             "amount": "1"
           },
           {
@@ -170,23 +161,15 @@ function injectDummyDB(){
         "note": "Make chicken curry less spicy",
         "location": "LatLng(18.78828218510149, 98.9929770532485)"
       },{
-        "_id": {
-          "$oid": "64258c9ec121f3bcd1236682"
-        },
         "id": 2,
         "customer": "Jane Doe",
-        "time": {
-          "$date": "2011-04-20T02:30:51.010Z"
-        },
+        "time": Date.now(),
         "address": "Robert Robertson, 1234 NW Bobcat Lane, St. Robert, MO 65584-5678",
         "telNo": "0832221155",
         "items": [
           {
             "name": "Chicken curry",
             "price": 60,
-            "_id": {
-              "$oid": "64252c7993e4cc92d4173993"
-            },
             "amount": "1"
           },
           {
